37122bf74d72a7789964c7c903e9c8368865fc24
Alexander Foxleigh <alex@alexfoxleigh.com>
2019-02-09 01:27:43 +0000

    adds legacy banner

---
+++ src/components/legacy-banner/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './styles.scss'
+
+const LegacyBanner = () => (
+  <section className="legacy-banner">
+    <h1>This article is a legacy article</h1>
+    <p>This blog was first built in 2010 and has gone through a lot of changes.
+     This article was written for a previous version so please excuse any graphical issues.
+    </p>
+    <p>
+      If you see any broken images or links however. Please let me know.
+    </p>
+  </section>
+)
+
+export default LegacyBanner
