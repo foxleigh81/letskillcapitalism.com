5df862757c19b15c72736f89c0bbacef90c04775
Alexander Foxleigh <alex@alexfoxleigh.com>
2019-02-10 16:41:09 +0000

    added meta data component

---
+++ src/components/article-meta-data/index.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import moment from 'moment'
+
+import './styles.scss'
+
+const MetaData = ({ author, date }) => {
+
+  const formatDate = (d) => {
+    const dateString = moment(d)
+    return dateString.format('Do MMMM YYYY')
+  }
+
+  return (
+    <div className="meta-data">
+      <span className="meta-data__author">Posted by: { author }</span>
+      <span className="meta-data__date"> on: { formatDate(date) }</span>
+    </div>
+  )
+}
+
+MetaData.propTypes = {
+  date: PropTypes.string.isRequired,
+  author: PropTypes.string,
+}
+
+MetaData.defaultProps = {
+  author: 'Alex Foxleigh',
+}
+
+export default MetaData
